861d3f5612fa2ae8336a3a58f9430f603ead7b4f
netakiryosuke <sigoryosuke@gmail.com>
2026-01-06 08:46:12 +0900

    Add PlayerShip.cs

---
+++ Assets/Scripts/PlayerShip/PlayerShip.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace PlayerShip
+{
+    public class PlayerShip : MonoBehaviour
+    {
+        [SerializeField] private InputAction moveInput;
+        [SerializeField] private InputAction slowMoveInput;
+        [SerializeField] private float normalSpeed = 4f;
+        [SerializeField] private float slowSpeed = 2f;
+        private Vector2 _moveInputValue;
+        private bool _isSlowMove;
+
+        private void OnEnable()
+        {
+            moveInput.performed += OnMove;
+            moveInput.canceled += OnMove;
+            moveInput.Enable();
+
+            slowMoveInput.started += OnSlowMove;
+            slowMoveInput.performed += OnSlowMove;
+            slowMoveInput.canceled += OnSlowMove;
+            slowMoveInput.Enable();
+        }
+        
+        private void Update()
+        {
+            Move();
+        }
+        
+        private void OnDisable()
+        {
+            moveInput.performed -= OnMove;
+            moveInput.canceled -= OnMove;
+            moveInput.Disable();
+            
+            slowMoveInput.started -= OnSlowMove;
+            slowMoveInput.performed -= OnSlowMove;
+            slowMoveInput.canceled -= OnSlowMove;
+            slowMoveInput.Disable();
+        }
+
+        private void OnMove(InputAction.CallbackContext context)
+        {
+            _moveInputValue = context.ReadValue<Vector2>();
+        }
+
+        private void OnSlowMove(InputAction.CallbackContext context)
+        {
+            _isSlowMove = context.ReadValueAsButton();
+        }
+        
+        private void Move()
+        {
+            if (_isSlowMove)
+            {
+                transform.position += new Vector3(_moveInputValue.x, _moveInputValue.y, 0) * (slowSpeed * Time.deltaTime);
+            }
+            else
+            {
+                transform.position += new Vector3(_moveInputValue.x, _moveInputValue.y, 0) * (normalSpeed * Time.deltaTime);
+            }
+        }
+    }
+}
+
