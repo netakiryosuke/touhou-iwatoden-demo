c0fb268d53c8a35e9c7bdc9a6861da8fcaa1d387
netakiryosuke <sigoryosuke@gmail.com>
2026-01-08 19:43:17 +0900

    Add MovePatternBase.cs

---
+++ Assets/Scripts/Enemy/MovePattern/Base/MovePatternBase.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Enemy.MovePattern.Base
+{
+    public class MovePatternBase : MonoBehaviour
+    {
+        public virtual void Initialize()
+        {
+            
+        }
+    }
+}
