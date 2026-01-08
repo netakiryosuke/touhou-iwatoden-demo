755ab50c25040b2b9e6ff20d91a80b1fa6b20858
netakiryosuke <sigoryosuke@gmail.com>
2026-01-08 19:32:57 +0900

    Add CsvLoader.cs

---
+++ Assets/Scripts/Enemy/Utils/CsvLoader.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Enemy.Utils
+{
+    public class CsvLoader
+    {
+        public static Queue<EnemyData> Load(TextAsset csv)
+        {
+            Queue<EnemyData> enemyDataQueue = new Queue<EnemyData>();
+
+            string[] lines = csv.text.Split('\n');
+
+            for (int i = 1; i < lines.Length; i++)
+            {
+                string[] columns = lines[i].Split(',');
+                enemyDataQueue.Enqueue(
+                    new EnemyData
+                    {
+                        SpawnTime = float.Parse(columns[0]),
+                        Position = new Vector3(float.Parse(columns[1]), float.Parse(columns[2]), 0),
+                        EnemyId = int.Parse(columns[3]),
+                        MovePatternId = int.Parse(columns[4]),
+                        DanmakuPatternId = int.Parse(columns[5]),
+                        Hp = float.Parse(columns[6]),
+                    }
+                );
+            }
+
+            return enemyDataQueue;
+        }
+    }
+}
