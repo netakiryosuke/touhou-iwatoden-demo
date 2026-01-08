c80b9e3b41a00c4f49d82bf9b4ada2beb89bfbb1
netakiryosuke <sigoryosuke@gmail.com>
2026-01-08 19:22:25 +0900

    Add EnemyData (DTO)

---
+++ Assets/Scripts/Enemy/EnemyData.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace Enemy
+{
+    public class EnemyData : MonoBehaviour
+    {
+        public float spawnTime;
+        public Vector3 position;
+        public int enemyId;
+        public int movePatternId;
+        public int danmakuPatternId;
+        public float hp;
+    }
+}
