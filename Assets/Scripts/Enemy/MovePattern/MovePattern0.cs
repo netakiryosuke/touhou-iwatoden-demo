c40190aea3f33e97d1dd4b9a4fb04445476c3208
netakiryosuke <sigoryosuke@gmail.com>
2026-01-08 20:55:10 +0900

    Minor changes

--- Assets/Scripts/Enemy/MovePattern/MovePattern0.cs
+++ Assets/Scripts/Enemy/MovePattern/MovePattern0.cs
@@ -5,14 +5,15 @@ namespace Enemy.MovePattern
 {
     public class MovePattern0 : MovePatternBase
     {
+        private float _speed;
         public override void Initialize()
         {
-            
+            _speed = 0.01f;
         }
         
         private void Update()
         {
-            transform.position += Vector3.down * Time.deltaTime;
+            transform.position += Vector3.down * _speed * Time.deltaTime;
         }
     }
 }
