68fe47ccde362b6e3b98c1b08b7dfd9600209a6d
netakiryosuke <sigoryosuke@gmail.com>
2026-01-13 20:26:45 +0900

    Add DanmakuPatternBase.cs

---
+++ Assets/Scripts/Enemy/DanmakuPattern/Base/DanmakuPatternBase.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Enemy.DanmakuPattern.Base
+{
+    public abstract class DanmakuPatternBase : MonoBehaviour
+    {
+        public abstract void Initialize();
+    }
+}
