33d47d8b18a0d447f3e25f2e6dcaf33c7cf6e2b6
netakiryosuke <sigoryosuke@gmail.com>
2026-01-14 20:32:22 +0900

    Add die and respawn feature

--- Assets/Scripts/PlayerShip/PlayerShip.cs
+++ Assets/Scripts/PlayerShip/PlayerShip.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -9,12 +10,20 @@ namespace PlayerShip
         [SerializeField] private InputAction slowMoveInput;
         [SerializeField] private float normalSpeed = 4f;
         [SerializeField] private float slowSpeed = 2f;
+        [SerializeField] private float respawnY = -3.5f;
+        [SerializeField] private float returnY = -2.5f;
         private Vector2 _moveInputValue;
         private bool _isSlowMove;
+        private bool _canControl = true;
+        private bool _isInvincible;
+        private SpriteRenderer _spriteRenderer;
 
         private void Update()
         {
-            Move();
+            if (_canControl)
+            {
+                Move();
+            }
         }
         
         private void OnEnable()
@@ -60,6 +69,56 @@ namespace PlayerShip
                 transform.position += new Vector3(_moveInputValue.x, _moveInputValue.y, 0) * (normalSpeed * Time.deltaTime);
             }
         }
+        
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (_isInvincible) return;
+
+            if (other.CompareTag("EnemyDanmaku") || other.CompareTag("Enemy"))
+            {
+                StartCoroutine(DieAndRespawn());
+            }
+        }
+
+        private IEnumerator DieAndRespawn()
+        {
+            _canControl = false;
+            _isInvincible = true;
+
+            // 一旦消す
+            gameObject.SetActive(false);
+            yield return null;
+
+            // リスポーン位置
+            transform.position = new Vector3(0f, respawnY, 0f);
+            gameObject.SetActive(true);
+
+            StartCoroutine(Blink());
+
+            // 上昇復帰
+            while (transform.position.y < returnY)
+            {
+                transform.position += Vector3.up * (2f * Time.deltaTime);
+                yield return null;
+            }
+
+            _canControl = true;
+
+            yield return new WaitForSeconds(2f);
+            _isInvincible = false;
+        }
+
+        private IEnumerator Blink()
+        {
+            while (_isInvincible)
+            {
+                _spriteRenderer.color = new Color(1, 1, 1, 0.3f);
+                yield return new WaitForSeconds(0.1f);
+                _spriteRenderer.color = Color.white;
+                yield return new WaitForSeconds(0.1f);
+            }
+
+            _spriteRenderer.color = Color.white;
+        }
     }
 }
-
