aad87f0df4ad12bf862dbad98d12476c33345bdb
netakiryosuke <sigoryosuke@gmail.com>
2026-01-15 21:26:50 +0900

    Fix parameter in PlayField.cs

--- Assets/Scripts/Utils/PlayField.cs
+++ Assets/Scripts/Utils/PlayField.cs
@@ -4,7 +4,7 @@ namespace Utils
 {
     public static class PlayField
     {
-        public static readonly Vector2 Min = new(-7f, -6f);
-        public static readonly Vector2 Max = new(7f, 6f);
+        public static readonly Vector2 Min = new(-6f, -5f);
+        public static readonly Vector2 Max = new(1f, 5f);
     }
 }
