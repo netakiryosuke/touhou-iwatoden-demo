723cc9c0d42859733d7e4c8e91f2b6bc662e389e
netakiryosuke <sigoryosuke@gmail.com>
2026-01-15 22:28:38 +0900

    Add GameManager.cs

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    [SerializeField] private int initialLife = 3;
+    private int _life;
+
+    private void Awake()
+    {
+        Instance = this;
+        _life = initialLife;
+    }
+
+    public void OnPlayerHit()
+    {
+        _life--;
+
+        // DanmakuClearer.ClearAll();
+
+        if (_life < 0)
+        {
+            GameOver();
+        }
+    }
+
+    private void GameOver()
+    {
+        // TODO
+        Debug.Log("Game Over");
+    }
+}
