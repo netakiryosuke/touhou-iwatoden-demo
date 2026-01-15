69124253243a1f813f6ead5d7bb19be8b2f85632
netakiryosuke <sigoryosuke@gmail.com>
2026-01-15 21:25:11 +0900

    Add PlayField.cs

---
+++ Assets/Scripts/Utils/PlayField.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Utils
+{
+    public static class PlayField
+    {
+        public static readonly Vector2 Min = new(-7f, -6f);
+        public static readonly Vector2 Max = new(7f, 6f);
+    }
+}
